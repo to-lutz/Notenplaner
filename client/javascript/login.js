abbdff4909c83ad7afe0dc3bc82a1cf8331fda1b
to-lutz <tobias.lutz07@gmail.com>
2024-10-24 17:40:23 +0200

    API Request

--- client/javascript/login.js
+++ client/javascript/login.js
@@ -6,3 +6,39 @@ document.querySelector("#togglePassword").addEventListener("click", (e) => {
         input.type = "password";
     }
 });
+
+document.querySelector("#sign-in").addEventListener("click", (e) => {
+    let name = document.querySelector("#name");
+    let password = document.querySelector("#password");
+
+    if (name.value.length == 0) {
+        name.style.border = "1px solid red";
+    } else {
+        name.style.border = "none";
+    }
+    if (password.value.length == 0) {
+        password.style.border = "1px solid red";
+    } else {
+        password.style.border = "none";
+    }
+
+    if (name.value.length != 0 && password.value.length != 0) {
+        loginRequest();
+    }
+});
+
+async function loginRequest(userName, password) {
+    const url = "/api/login";
+    try {
+        const response = await fetch(url, {method: "POST"});
+
+        if (!response.ok) {
+            return false;
+        } else {
+            return true;
+        }
+    } catch (error) {
+        console.error(error.message);
+        return false;
+    }
+}
--- server/routes/api/loginAPI.js
+++ server/routes/api/loginAPI.js
@@ -4,7 +4,22 @@ var path = require('path');
 
 /* POST login authorization. */
 router.post('/', function(req, res, next) {
-    res.status(200).send("Authorized");
+    /*
+    res.status(200).json({
+        status: 'Authorized',
+        message: 'Eingeloggt als NAME',
+        id: -1,
+        name: 'NAME',
+        date: new Date()
+    });
+    */
+    res.status(403).json({
+        status: 'Unauthorized',
+        message: 'Falscher Benutzername oder Passwort.',
+        id: -1,
+        name: '',
+        date: new Date()
+    });
 });
 
 module.exports = router;
