2122085e53ac942761467ac1ffa252a4acfdb314
to-lutz <tobias.lutz07@gmail.com>
2024-10-24 14:46:50 +0200

    Add favicon

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
 
 var indexRouter = require('./routes/index');
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client')));
+app.use(favicon(path.join(__dirname, '../client/images/favicon', 'favicon.ico')));
 
 app.use('/', indexRouter);
 
