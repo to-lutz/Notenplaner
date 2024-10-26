327a15d8040cbed3069d1aa3f1f92c65790f7299
to-lutz <tobias.lutz07@gmail.com>
2024-10-26 19:04:00 +0200

    Add sessionid to table

--- server/routes/api/loginAPI.js
+++ server/routes/api/loginAPI.js
@@ -18,16 +18,23 @@ router.post('/', function (req, res, next) {
     connection.query('SELECT * FROM users.user WHERE username="' + req.body.username +'" AND password="' + req.body.password + '"', function (err, rows, fields) {
         if (err) throw err;
         if (rows.length != 0) {
+            let sessID = new Date().getTime() * rows[0].id;
+            let username = rows[0].username;
+            let id = rows[0].id;
+            connection.query("INSERT INTO `users`.`sessionids`(`sessionid`,`username`,`userid`) VALUES(" + sessID +",'" + rows[0].username + "'," + rows[0].id + ");", (err, rows, fields) => {
+                if (err) throw err;
+                
+                res.status(200).json({
+                    status: 'Authorized',
+                    message: 'Eingeloggt als ' + username,
+                    id: id,
+                    name: username,
+                    date: new Date(),
+                    sessionID: sessID,
+                });
 
-            res.status(200).json({
-                status: 'Authorized',
-                message: 'Eingeloggt als ' + rows[0].username,
-                id: rows[0].id,
-                name: rows[0].username,
-                date: new Date(),
-                sessionID: -1
+                connection.end();
             });
-
         } else {
 
             res.status(403).json({
@@ -39,10 +46,12 @@ router.post('/', function (req, res, next) {
                 sessionID: -1,
             });
 
+            connection.end();
+
+
         }
     });
-    
-    connection.end();
+
 });
 
 module.exports = router;
