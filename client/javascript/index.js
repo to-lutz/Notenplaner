ea4d72f7c247fdc02dc6ad08350d53f7dc7aca8b
to-lutz <tobias.lutz07@gmail.com>
2024-10-26 19:47:25 +0200

    API request to get data by sessionid

--- client/javascript/index.js
+++ client/javascript/index.js
@@ -3,8 +3,37 @@ let sessionID = getCookie("np_session_id");
 if (sessionID == null || sessionID.length == 0) {
     window.location.href = "/login";
 } else {
-    // Get database data
-    
+    let apiCall = async () => {
+        // Get database data
+        const url = "/api/sessionid";
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(
+                    {
+                        sessionid: sessionID
+                    }
+                ),
+            });
+
+            response.json().then((data) => {
+                if (data.status == "Known") {
+                    document.querySelectorAll("#account-name").forEach((e) => {
+                        e.innerHTML = data.name;
+                    })
+                } else {
+                    document.cookie = 'np_session_id=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+                    window.location.href = "/login";
+                }
+            });
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+    apiCall();
 }
 
 function getCookie(name) {
