0a25d3784d5f82afc78f8addd51b5860b553a8ec
to-lutz <tobias.lutz07@gmail.com>
2024-10-29 13:43:59 +0100

    Add setting page

---
+++ client/javascript/settings.js
@@ -0,0 +1,75 @@
+// Logged in Check
+let sessionID = getCookie("np_session_id");
+if (sessionID == null || sessionID.length == 0) {
+    window.location.href = "/login";
+} else {
+    let apiCall = async () => {
+        // Get database data
+        const url = "/api/sessionid";
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(
+                    {
+                        sessionid: sessionID
+                    }
+                ),
+            });
+
+            response.json().then((data) => {
+                if (data.status == "Known") {
+                    document.querySelectorAll("#account-name").forEach((e) => {
+                        e.innerHTML = data.name;
+                    })
+                    userID = data.id;
+                    fetchDurchschnitt();
+                    fetchHighestSubjects();
+                    fetchGrades();
+                } else {
+                    document.cookie = 'np_session_id=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+                    window.location.href = "/login";
+                }
+            });
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+    apiCall();
+}
+
+function getCookie(name) {
+    function escape(s) { return s.replace(/([.*+?\^$(){}|\[\]\/\\])/g, '\\$1'); }
+    var match = document.cookie.match(RegExp('(?:^|;\\s*)' + escape(name) + '=([^;]*)'));
+    return match ? match[1] : null;
+}
+
+let userID = undefined;
+
+document.querySelector("#sign-out").addEventListener("click", (e) => {
+    document.cookie = 'np_session_id=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    window.location.href = "/login";
+
+    let apiCall = async () => {
+        // Get database data
+        const url = "/api/sessionidremove";
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(
+                    {
+                        sessionid: sessionID
+                    }
+                ),
+            });
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+    apiCall();
+});
