414746815e03833179fd768a9c7cfdcd5d5be50f
to-lutz <tobias.lutz07@gmail.com>
2024-11-15 17:02:21 +0100

    Add updateFachAPI Endpoint

---
+++ server/routes/api/updateFachAPI.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var crypto = require('crypto');
+require('dotenv').config();
+
+
+/* POST update fach. */
+router.post('/', function (req, res, next) {
+
+    var connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS
+    });
+
+    connection.connect();
+
+
+    connection.query('UPDATE notenplaner.faecher SET ' + req.body.setting + '="' + req.body.value + '" WHERE userid="' + req.body.userid + '" AND fachid="' + req.body.fachid + '"', function (err, rows, fields) {
+        if (err) {
+            throw err;
+        }
+
+        res.status(200).json({
+            status: 'Updated',
+            id: req.body.userid,
+            fachid: req.body.fachid,
+            date: new Date(),
+        });
+
+        connection.end();
+    });
+
+});
+
+module.exports = router;
