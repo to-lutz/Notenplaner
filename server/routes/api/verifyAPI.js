700a98261c23e2558936748007897576cb2c4b63
to-lutz <tobias.lutz07@gmail.com>
2024-11-16 16:39:00 +0100

    Remove unused function

--- server/routes/api/verifyAPI.js
+++ server/routes/api/verifyAPI.js
@@ -42,32 +42,4 @@ router.get('/', function (req, res, next) {
 
 });
 
-
-let updateAbiFach = function (fachid, userid, abiturid) {
-    var connection = mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS
-    });
-
-    connection.connect();
-
-    let afb;
-    connection.query('SELECT anforderungsbereich FROM notenplaner.faecher WHERE userid=' + userid + ' AND id=' + fachid, (err, rows, fields) => {
-        afb = rows[0].anforderungsbereich;
-
-        connection.query('UPDATE notenplaner.abitur SET fachid="' + fachid + '", anforderungsbereich="' + afb + '" WHERE userid="' + userid + '" AND abiturfachid="' + abiturid + '"', function (err, rows, fields) {
-            if (err) throw err;
-            if (rows.length != 0) {
-
-                connection.end();
-            } else {
-
-                connection.end();
-            }
-        });
-    });
-
-}
-
 module.exports = router;
