065491dcdde7c8a72d368c8810fd5741fbb1f9df
to-lutz <tobias.lutz07@gmail.com>
2024-11-16 14:32:07 +0100

    Set abitur fach 1 to profilfach when setting

--- server/routes/api/updateFachAPI.js
+++ server/routes/api/updateFachAPI.js
@@ -22,6 +22,12 @@ router.post('/', function (req, res, next) {
             throw err;
         }
 
+        if (req.body.setting == "isProfilfach" && req.body.value == true) {
+            // TODO: Check for previous profilfach and set that to false
+
+            updateAbiFach(req.body.fachid, req.body.userid, 0);
+        }
+
         res.status(200).json({
             status: 'Updated',
             id: req.body.userid,
@@ -33,5 +39,29 @@ router.post('/', function (req, res, next) {
     });
 
 });
+
+
+let updateAbiFach = function (fachid, userid, abiturid) {
+    var connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS
+    });
+
+    connection.connect();
+
+    connection.query('UPDATE notenplaner.abitur SET fachid="' + fachid + '" WHERE userid="' + userid +'" AND abiturfachid="' + abiturid + '"', function (err, rows, fields) {
+        if (err) throw err;
+        if (rows.length != 0) {
+
+            // TODO: Get Anforderungsbereich and Set
+
+            connection.end();
+        } else {
+
+            connection.end();
+        }
+    });
+}
 
 module.exports = router;
